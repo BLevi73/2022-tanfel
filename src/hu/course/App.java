7f556a9af76e545c5019f6e72b4d68041d464940
benol17 <benolevi@gmail.com>
2022-01-12 09:25:09 +0100

    final program

--- src/hu/course/App.java
+++ src/hu/course/App.java
@@ -1,17 +1,21 @@
 package hu.course;
 
 import hu.course.controller.CourseService;
-import hu.course.model.service.DataApi;
-import hu.course.model.service.DataParser;
-import hu.course.model.service.FileReader;
+import hu.course.model.service.*;
+
+import java.util.Scanner;
 
 public class App {
 
     private final CourseService courseService;
+    private final Console console;
+    private final DataWriter dataWriter;
 
     private App() {
         DataApi dataApi= new DataApi(new FileReader(), new DataParser());
         courseService = new CourseService(dataApi.getData("beosztas.txt"));
+        console = new Console(new Scanner(System.in));
+        dataWriter = new DataWriter("of.txt");
     }
 
     public static void main(String[] args) {
@@ -21,6 +25,21 @@ public class App {
     private void run() {
         System.out.println("2.feladat:");
         System.out.println("A fájlban " + courseService.getCourseCount() + " bejegyzés van.");
+        System.out.println("3.feladat;");
+        System.out.println("Az iskolában a heti óraszám: " + courseService.getTotalLessonCount());
+        System.out.println("4.feladat:");
+        System.out.print("Egy tanár neve: ");
+        String name = console.read();
+        System.out.println("A tanár heti óraszáma: " + courseService.getTeacherWeeklyLesson(name));
+        dataWriter.write(courseService.getMasterTeachers());
+        System.out.println("6.feladat:");
+        System.out.print("Osztály: ");
+        String classId = console.read();
+        System.out.print("Tantárgy: ");
+        String subject = console.read();
+        System.out.println(courseService.getCourseStatus(subject, classId));
+        System.out.println("7.feladat: ");
+        System.out.println("Az iskolában " + courseService.getTeacherCount() + " tanár tanít.");
     }
 
 }
--- src/hu/course/controller/CourseService.java
+++ src/hu/course/controller/CourseService.java
@@ -3,6 +3,7 @@ package hu.course.controller;
 import hu.course.model.domain.Course;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CourseService {
 
@@ -14,5 +15,44 @@ public class CourseService {
 
     public int getCourseCount() {
         return courses.size();
+    }
+
+    public int getTotalLessonCount() {
+        return courses.stream()
+                .mapToInt(Course::getWeeklyLessonNumber)
+                .sum();
+    }
+
+    public int getTeacherWeeklyLesson(String name) {
+        return courses.stream()
+                .filter(i -> i.getTeacher().equals(name))
+                .mapToInt(Course::getWeeklyLessonNumber)
+                .sum();
+    }
+
+    public List<String> getMasterTeachers() {
+        return courses.stream()
+                .filter(i -> "osztalyfonoki".equals(i.getSubject()))
+                .map(course -> String.format("%s - %s)", course.getClassId(),course.getTeacher()))
+                .collect(Collectors.toList());
+    }
+
+    public String getCourseStatus(String subject, String classId) {
+        return getCertainCourseCount(subject, classId) == 1
+                ? "Osztályszinten tanulják."
+                : "Csoportbontásban tanulják.";
+    }
+
+    private long getCertainCourseCount(String subject, String classId) {
+        return courses.stream()
+                .filter(i -> i.getSubject().equals(subject) && i.getClassId().equals(classId))
+                .count();
+    }
+
+    public long getTeacherCount() {
+        return courses.stream()
+                .map(Course::getTeacher)
+                .distinct()
+                .count();
     }
 }
